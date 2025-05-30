0fedb8a16b08d754a43faaf7cc8bf3d03bd3c9fa
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-05-31 02:51:40 +0530

    set up db backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,24 +2,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const session = require("express-session");
-const bcrypt = require("bcryptjs");
-const path = require('path');
-
 const app = express();
+const connectToDb = require('./db/db');
+
+connectToDb();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('../Frontend'));
-app.use('/static', express.static('static'));
 
-// View engine setup
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+// const bodyParser = require("body-parser");
+// const mongoose = require('mongoose');
+// const session = require("express-session");
+// const bcrypt = require("bcryptjs");
+// const path = require('path');
 
 // Routes
 app.get('/', (req, res) => {
