2654a5d90b43efa24c8f8c590e96f6964a9cb58a
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-06-01 00:08:11 +0530

    updated for unity frontend connection

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
